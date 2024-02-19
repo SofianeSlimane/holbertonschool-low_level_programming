7dd85513fe7cc1409ad41185f32f422a5440ce24
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-19 13:04:35 +0000

    test

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+printf("%lu\n","Size of a char:" sizeof(char));
+printf("%lu\n","Size of an int" sizeof(int));
+printf("%lu\n","Size of a long int" sizeof(long int));
+printf("%lu\n","Size of a long long" sizeof(long long int));
+printf("%lu","Size of a float" sizeof(float));
+return 0;
+}                                                           
