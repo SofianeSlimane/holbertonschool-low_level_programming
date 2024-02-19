a54d446f2f19a6a854ac02a5a88a033b8cfbfa5d
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-19 14:23:48 +0000

    tEST

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* Commentaire */
+/**  
+ * main - Entry point                                                                                  
+ *                                                                                                     
+ * Return: Always 0 (Success)                                                                          
+ */
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
