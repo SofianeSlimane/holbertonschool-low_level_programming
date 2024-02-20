82da5b248a0a5f900e2a32e938a3714bf483916b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-20 20:36:57 +0000

    Patience

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '9')
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(i);
+}
+}
+return (0);
+}
