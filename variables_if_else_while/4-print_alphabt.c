f90a0bd8e867709f8990c584055d5d211247d72c
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-20 12:44:41 +0000

    4 alphabet

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**  
- * main - Entry point  
+/**
+ * main - Entry point
  *
-
  * Return: Always 0 (Success)
-
  */
 int main(void)
 {
@@ -19,6 +17,6 @@ else
 putchar(i);
 }
 }
-putchar('\n'); 
+putchar('\n');
 return (0);
 }
