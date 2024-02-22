7d27e9484d67e6fd5370f138ae9a34c6cc1190aa
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-22 13:46:12 +0000

    test

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int r)
+{
+  r = 'A';
+  if (r == 'a')
+    {
+      return (1);
+    }
+  else if (r == 'A')
+    {
+      return (0);
+    }
+  return (0);
+}
+  
