85e671c5c9e0c34d4bd0ac30d5ac00c8e8d76fd4
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-22 19:35:32 +0000

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _abs(int r)
+{
+  if (r < 0)
+    {
+      r = r * -1;
+      printf("%d\n", r);
+    }
+  else
+    {
+      printf("%d\n", r);
+    }
+  return (0);
+}
