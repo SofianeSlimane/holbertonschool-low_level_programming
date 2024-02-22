cd42f1a85be611a10b77adaf32ba9907cd896282
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-22 20:29:21 +0000

    test

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int r)
+{
+  if (r <= 0 || r >= 0)
+    {
+  r = r % 10;
+    } 
+
+return (r);
+}  
