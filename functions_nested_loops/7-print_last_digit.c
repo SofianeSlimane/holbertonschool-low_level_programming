e75cb6fbad546de2eb7a69a7d7dcc8e66b73a44d
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-23 08:48:39 +0000

    test

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -11,9 +11,7 @@ int print_last_digit(int r)
   if (r >= 0 || r <= 0)
     {
       r = r % 10;
-    }
-   
-    
-
-return (r);
-}  
+      printf("%d", r);
+	}
+  return (r);
+}
