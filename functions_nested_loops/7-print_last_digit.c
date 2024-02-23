fe87bb4f3a3b6f13447d9623c641406af1a09456
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-23 10:48:34 +0000

    test

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,29 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_last_digit - check the code
+ * @r: value
  *
  * Return: Always 0.
  */
 int print_last_digit(int r)
 {
-  if (r >= 0)
-    {
-   r = r%10;
-  _putchar('0' + r);
-  
-    }
-  else if (r < 0)
-    {
-      r = r%10 * (-1);
-      
-      _putchar('0' + r);
-      
-    }
-  return (r);
+if (r >= 0)
+{
+r = r % 10;
+_putchar('0' + r);
 }
-   
-    
-  
-
- 
+else if (r < 0)
+{
+r = r % 10 * (-1);
+_putchar('0' + r);
+}
+return (r);
+}
