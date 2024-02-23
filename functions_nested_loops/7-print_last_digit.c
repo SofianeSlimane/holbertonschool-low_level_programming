d44d9addd0de86f1c14876213958a0daefecfeb1
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-23 10:10:11 +0000

    test

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,10 @@
  */
 int print_last_digit(int r)
 {
-  
-  if (r < 0)
-    {
-      r = r%10;
-      return (r);
-    }
-  return(r);
+   r = r%10;
+  _putchar('0' + r);
+  return (r);
+    
   
 }
+ 
