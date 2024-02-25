006977c4287142cd5d1cde2449d2eab19be6554b
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-25 15:37:34 +0000

    test

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void  times_table(void)
   int i;
   int a = 0;
   int b = 0;
-  for (i = 0; i <= 11; i++)
+  for (i = 0; i <= 10; i++)
     {
       if (i <= 10)
 	{
@@ -21,10 +21,15 @@ void  times_table(void)
 	  _putchar(',');
 	  _putchar(' ');
 	  _putchar(' ');
+	  a++;
+	  b++;
+	    
 	}
       else if (i == 11)
 	{
 	  _putchar('\n');
+	  a++;
+	  b++;
 	}
     }
 }
