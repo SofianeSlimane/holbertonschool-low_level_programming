9f452fe36184f0fcb1c668e3e61edd40151e1d70
root <sofianeslimane24@gmail.com>
2024-02-26 14:32:12 +0000

    test

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,17 @@
  */
 void print_line(int n)
 {
-    
+  int i = 0;
+  while (i <= n)
+    {
+      
   if (n <= '0')
     {
       _putchar('\n');
     }
   else
     {
-       _putchar(n * '_');
+       _putchar('_');
     }
+}
 }
