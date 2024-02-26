a43e35b5708d6d2dad7143819a3ca2dff865ac26
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-26 10:53:25 +0000

    test

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_most_numbers - check the code
  *
  * Return: Always 0.
  */
 void print_most_numbers(void)
 {
-  int i;
-  for (i ='0'; i <= '9'; i++)
-  {
- 
-    if ( i == '2' || i == '4')
-      {
-      }
-    else
-      {
-	_putchar(i);
-
-      }
-   
-  }
- _putchar('\n');
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i == '2' || i == '4')
+{
+}
+else
+{
+_putchar(i);
+}
+}
+_putchar('\n');
 }
