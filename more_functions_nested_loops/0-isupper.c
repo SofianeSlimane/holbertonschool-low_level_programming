6b77476b5111804be4721637a50e0aaf2287fca8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-26 09:53:04 +0000

    test

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * _isupper - check the code.
+ * @c: value that need to be checked to know if it's upper case
  *
  * Return: Always 0.
  */
 int _isupper(int c)
-
 {
-  if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
 }
-  
+else
+{
+return (0);
+}
+}
