fbbf723abda4b8d9fbc89919c807c491f041fd78
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-26 10:31:31 +0000

    test

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+  int i;
+for (i = '0'; i <= '9'; i++)
+  {
+ 
+    if ( i == '2' || i == '0')
+      {
+      }
+    else
+      {
+	_putchar(i);
+
+      }
+    _putchar('\n');
+  }
+}
