c5d1c9a6c02fd91c928000a81be7bf835c422fe2
root <sofianeslimane24@gmail.com>
2024-02-26 13:53:25 +0000

    test

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+  n = _putchar('_');
+  if (n <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      _putchar(n);
+}
+}
