020da43d5845df6acca2ad2e9e784dcc91a4fc1e
root <sofianeslimane24@gmail.com>
2024-02-26 12:59:46 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,21 @@
 void more_numbers(void)
 {
 int i;
-int j;
+ int j;
 for (i = 0; i <= 10; i++)
 {
-  for (j = '0'; j <= 14; j++)
+  for (j = '0'; i < 14 % 10; j++ )
     {
-    if (j >= '0')
-      {
-	_putchar(j + '0');
-      }
-    else if ( j >= 10 && j <= 14)
-      {
-	_putchar(j % 10 + '0');
-      }
+      if (j <= '0')
+	{
+	  _putchar(j + '0');
+	}
+      else if (j >= 10 && j <= 14)
+	{
+	  _putchar(j % 10 + '0');
+	}
+      _putchar('\n');
     }
-
  }
-_putchar('\n');
+ 
 }
-
