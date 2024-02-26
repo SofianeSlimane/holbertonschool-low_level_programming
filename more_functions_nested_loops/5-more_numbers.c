3d9ccd3e483de60725b308ee175158dd1d07e4f6
root <sofianeslimane24@gmail.com>
2024-02-26 13:28:52 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,16 +14,13 @@ for (i = 0; i <= 10; i++)
 {
   for (j = 0; j <= 14; j++ )
     {
-      if (j < 10)
+      if (j >= 10)
 	{
+       _putchar('1');
      _putchar(j % 10 + '0');
     }
-      else if (j >= 10 && j <= 14)
-	{
-	  _putchar((j/10) + '0');
-	  _putchar((j % 10) + '0');
-	}
- 
     }
  }
 }
+      
+     
