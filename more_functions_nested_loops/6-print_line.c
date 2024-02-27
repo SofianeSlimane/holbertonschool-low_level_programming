d0b6db285b736ead4a0287eee8277bfdf3cfb202
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-27 08:19:18 +0000

    test

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_line - check the code
+ * @n: number
  *
  * Return: Always 0.
  */
 void print_line(int n)
 {
-  int i;
-      if (n <= 0)
-	{
-	  
-	}
-      else
-	{
-	  for (i = 0; i < n; i++)
-	    {
-	  _putchar('_');
-	    }
-	}
-      _putchar('\n');
-     
+int i;
+if (n <= 0)
+{
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
 }
 
