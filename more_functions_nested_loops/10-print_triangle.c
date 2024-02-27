0a6b8f171538ee0da6b25b28225d7fa3b94d76cc
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-27 14:21:16 +0000

    test

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+  int i;
+  int j;
+
+  for (i = 0; i < size; i++)
+    {
+      for (j = 0; j < size; j++)
+	{
+	  _putchar('#');
+	}
+      _putchar('\n');
+    }
+}
