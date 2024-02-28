a57de104f10bbd7e632d1abae2fd1aea767327f6
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-28 22:58:51 +0000

    test

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+  int i;
+  for (i = 0; str[i] != '\0'; i++)
+    {
+      if (i % i == 0)
+	{
+      _putchar(str[i]);
+    }
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
