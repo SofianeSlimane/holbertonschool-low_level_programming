037c4047e1af00544719e7b9515e4398a7d12494
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-28 18:19:27 +0000

    test

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+  int i;
+  for (i = 0; i < *s; i++)
+    {
+      printf("%d", *s);
+    }
+  return (i);
+}
