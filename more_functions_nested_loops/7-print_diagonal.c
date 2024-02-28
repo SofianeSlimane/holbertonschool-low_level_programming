074453219c15d4b466d1c3b1caeedd341efa97ed
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-28 08:22:45 +0000

    test

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,30 +7,26 @@
  */
 void print_diagonal(int n)
 {
-  
   int i;
   int j;
-  
- 
   if (n <= 0)
     {
       _putchar('\n');
     }
-  
   else
     {
-      
-      for (i = 0; i < n; i++)
+  for (i= 0; i < n; i++)
+    {
+      for (j = 0; j < 1; j++)
 	{
-	  for (j = 0; j < n ; j++)
-	    {
-	      _putchar('\\');
-	      _putchar('\n');
-	      _putchar(' ');
-	    }
+	  _putchar('\\');
 	}
+      _putchar(' ');
+    }
     }
 }
+	      
+	  
 	     
                
        
