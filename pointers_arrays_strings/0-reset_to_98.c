74c5324511c275037c51c9fd5c0be801894a1d38
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-28 14:13:59 +0000

    test

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,17 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
+ * reset_to_98 - check the code
+ * @n: pointer
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-  
-  *n = 98;
-  
-   
-    
-  
-  
+*n = 98;
 }
