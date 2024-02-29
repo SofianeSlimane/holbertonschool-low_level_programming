4cc635f8a6de12ee46697033fb7ecc31cebe2a01
SofianeSlimane <sofianeslimane24@gmail.com>
2024-02-29 18:18:44 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
   for (taille = 0 ; s[taille] != '\0'; i++)
     {
     }
-  for (i = taille - 1; i < taille; i--)
+  for (i = taille - 1; i < s[taille]; i--)
     {
       _putchar(s[i]);
     }
