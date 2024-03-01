4811e32e6d133611916d6bf97bb81f72fda1953e
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-01 12:21:59 +0000

    test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+  int taille;
+  int i;
+  for (taille = 0, s[taille] != '\0'; taille++)
+    {
+    }
+  for (i = taille -1; i != '\0'; i--)
+    {
+    }
+  
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-typedef
 void reset_to_98(int *n);
 int _strlen(char *s);
 int _putchar(char c);
@@ -8,3 +7,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
