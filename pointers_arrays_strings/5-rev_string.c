79e3117286f2c7b51be80301b435053799a95002
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-01 13:26:54 +0000

    test

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,10 +4,10 @@ void rev_string(char *s)
 {
   int taille;
   int i;
-  for (taille = 0, s[taille] != '\0'; taille++)
+  for (taille = 0; s[taille] != '\0'; taille++)
     {
     }
   for (i = taille -1; i != '\0'; i--)
     {
     }
-  
+}
