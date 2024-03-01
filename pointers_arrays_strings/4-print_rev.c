f75d9bd177677e3f5cddd9cf68f95255780a55a4
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-01 12:07:56 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_rev - check the code
+ *@s: chaine de caracteres
  *
- * Return: Always 0.
  */
+
 void print_rev(char *s)
 {
-  int i ;
-  int taille;
-  
-  for (taille = 0 ; s[taille] != '\0'; taille++)
-    {
-    }
-  if (taille > 0)
-    {
-  for (i = taille - 1 ; i != '\0'; i--)
-    {      
-	  _putchar(s[i]);
-	}
-      
-  _putchar(s[i]);
-    }
-  _putchar('\n');
-}
+int i;
+int taille;
 
- 
+for (taille = 0 ; s[taille] != '\0'; taille++)
+{
+}
+if (taille > 0)
+{
+for (i = taille - 1 ; i != '\0'; i--)
+{
+_putchar(s[i]);
+}
+_putchar(s[i]);
+}
+_putchar('\n');
+}
