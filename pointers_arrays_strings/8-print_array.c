7fd1e9f4e9ccf056c0879b7a2b60964b86f6750d
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-02 13:19:18 +0000

    test

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,29 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
+ * print_array - check the code for
+ * @a: pointeur tableau
+ * @n: nombre elements tableau
  *
- * Return: Always 0.
  */
 
 void print_array(int *a, int n)
 {
-  int i;
-  for (i = 0; i < n; i++)
-    {
-      if (i == n - 1)
-	{
-	  printf("%d", a[i]);
-	    }
-      else
-	{
-      printf("%d", a[i]);
-      printf(",");
-      printf(" ");
-	}
-    }
-  printf("\n");
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d", a[i]);
 }
-  
-
-
+else
+{
+printf("%d", a[i]);
+printf(",");
+printf(" ");
+}
+}
+printf("\n");
+}
