9b436695269fa51d1f814421f48d123f712c09a6
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-02 13:46:48 +0000

    test

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+char *_strcpy(char *dest, char *src)
+{
+  int i;
+  int j;
+  for (i = 0; src[i] != '\0' || src[i] == '\0'; i++)
+    {
+    }
+  
+  dest = src;
+  for (j = 0; j <= i; j++)
+	 {
+	   _putchar(dest[j]);
+	 }
+  return (dest);
+}
