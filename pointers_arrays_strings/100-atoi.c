6d61211746197f228c3d5056e8e8cf8abf60d8c2
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-02 16:17:10 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,13 +12,13 @@ int _atoi(char *s)
   int i;
   for (i = 0; s[i] != '\0'; i++)
     {
-      if (!(s[i] >= '0' && s[i] <= '9'))
+      if (s[i] >= '0' && s[i] <= '9')
 	{
-	  return (0);
+	  _putchar(s[i]);
 	}
     }
     
-  return (s[i]);
+  return (0);
     }
   
   
