57c4f5c4770939e01ece8730c0b4eab4ad5fb053
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-03 14:59:49 +0000

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
-
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: valeur pointee par dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-  int i;
-  int taille;
-  for (taille = 0; src[taille] != '\0'; taille++)
-    {
-    }
-
-  for (i = 0; i <= taille; i++)
-    {
-      dest[i] = src[i];
-      
-    }
-  return (dest);
+int i;
+int taille;
+for (taille = 0; src[taille] != '\0'; taille++)
+{
 }
- 
+for (i = 0; i <= taille; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
