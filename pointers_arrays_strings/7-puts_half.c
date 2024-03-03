28e8fde2d991d8bc4c02784f70cc64c59cdc03f7
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-03 14:05:26 +0000

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,39 +1,33 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * puts_half - check the code
+ *@str: chaine de caracteres
  *
- * Return: Always 0.
  */
 
 void puts_half(char *str)
 {
-  int i;
-  int taille;
-  int j;
-
-  for (taille = 0; str[taille] != '\0'; taille++)
-    {
- 
-    }
-  if (taille % 2 == 0)
-    {
-      for (i = (taille / 2) ; str[i] != '\0'; i++)
-	 
-    {
-      _putchar(str[i]);
-    }
-      _putchar('\n');
-    }
-  else if (taille % 2 != 0)
-    {
-      for (j = (taille / 2) + 1; str[j] != '\0'; j++)
-	{
-	  _putchar(str[j]);
-	}
-      _putchar('\n');
-    }
+int i;
+int taille;
+int j;
+for (taille = 0; str[taille] != '\0'; taille++)
+{
 }
-	
-	 
-	
+if (taille % 2 == 0)
+{
+for (i = (taille / 2) ; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
+else if (taille % 2 != 0)
+{
+for (j = (taille / 2) + 1; str[j] != '\0'; j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
+}
