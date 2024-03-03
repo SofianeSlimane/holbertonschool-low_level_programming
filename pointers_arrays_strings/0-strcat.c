a59e85b769d7d33dc0b0b50176c184a0ef9e0ab1
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-03 16:13:18 +0000

    test

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,7 @@ char *_strcat(char *dest, char *src)
 
   int i;
   int taille;
+  char *ptr = dest;
 
   for (taille = 0; dest[taille] != '\0'; taille++)
     {
@@ -16,5 +17,5 @@ char *_strcat(char *dest, char *src)
       dest[taille + 1] += src[i];
     }
   
-  return (src);
+  return (ptr);
 }
