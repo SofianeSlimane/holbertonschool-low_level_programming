84394cfb315ac3fc5f47ae9bb050fedc670497db
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-03 19:29:59 +0000

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,36 +1,30 @@
 #include "main.h"
 
+/**
+ * _strcmp - compare chaine de caracteres
+ * @s1: chaine de caracteres
+ * @s2: chaine de caracteres
+ *
+ * Return: 0
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-
-  int taille1;
-  int taille2;
-  int j;
-  for (taille1 = 0; s1[taille1] != '\0'; taille1++)
-    {
-    }
-  for (taille2 = 0; s2[taille2] != '\0'; taille2++)
-    {
-    }
-
-
-
-
-  for (j = 0; s1[j] == '\0' || s2[j] == '\0'; j++)
-    {
-      if ((s1[j] =! s2[j]))
-	{
-	  return (s1[j] - s2[j]);
-	}
-      else if (s1[j] < s2[j])
-	{
-	  return (-15);
-	}
-      else if (s1[j] > s2[j])
-	{
-	  return (15);
-	}
-      
-    }
-  return (0);
+int j;
+for (j = 0; s1[j] != '\0' || s2[j] != '\0'; j++)
+{
+if (s1[j] != s2[j])
+{
+return (s1[j] - s2[j]);
+}
+else if (s1[j] < s2[j])
+{
+return (-15);
+}
+else if (s1[j] > s2[j])
+{
+return (15);
+}
+}
+return (0);
 }
