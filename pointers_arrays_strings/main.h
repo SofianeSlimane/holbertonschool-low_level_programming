3e4a1f133555d5186fbf8b4f98a4eab31eb3ccb9
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-04 10:51:09 +0000

    2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,4 +12,4 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
