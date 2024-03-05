e0dc98bde6d01467adfd7622fe080ed429279305
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-05 09:10:18 +0000

    3 length of prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
+/**
+ * _strspn - returns length of substring
+ * @s: string
+ * @accept: characters to be found
+ * Return: index of substring made of accept
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
-  unsigned int i;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      if (s[i] == accept)
-	{
-	  s[i] += accept;
-	}
-    }
-  return(i);
+unsigned int i;
+while (s[i] != *accept)
+{
+i++;
 }
-  
+return (i + 1);
+}
