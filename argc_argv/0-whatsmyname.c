a808e567795eb4e101555c53027cab5d1c07782c
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-09 14:48:08 +0000

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,11 +7,10 @@
  * Return:0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-if (argc > 0)
-{
+
 printf("%s\n", argv[0]);
-}
+
 return (0);
 }
