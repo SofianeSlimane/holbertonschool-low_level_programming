a0ff93ff0632baff21ee2fb88acb3cadffb72f5f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-12 10:10:05 +0000

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,24 +10,22 @@ char *_strdup(char *str)
 {
 int i;
 char *str2;
- int j;
- for (j = 0; str[j] != '\0'; j++)
-   {
-   }
-str2 = malloc(j * sizeof(char));
- if (str2 == NULL)
-   
-   {
-     return (NULL);
-   }
- else
-   {
- for (i = 0; i <= j - 1; i++)
+int j;
+for (j = 0; str[j] != '\0'; j++)
+{
+}
+str2 = malloc(sizeof(char) * j + 1);
+if (str == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i <= j; i++)
 {
 str2[i] = str[i];
 }
-   }
-
+}
 return (str2);
 free(str2);
 }
