0f3e45b55c381834481476aea3a2f3024fff2c5c
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-12 11:03:22 +0000

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,20 +11,17 @@ char *_strdup(char *str)
 int i;
 char *str2;
 int j;
-for (j = 0; str[j] != '\0'; j++)
-{
-}
-str2 = malloc(sizeof(char) * j + 1);
 if (str == NULL)
 {
 return (NULL);
 }
-else
+for (j = 0; str[j] != '\0'; j++)
 {
+}
+str2 = malloc(sizeof(char) * j + 1);
 for (i = 0; i <= j; i++)
 {
 str2[i] = str[i];
-}
 }
 if (str2 == NULL)
 {
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,6 +8,34 @@ char *str_concat(char *s1, char *s2)
 {
   
   int i;
-  int *s3;
+  int k;
+  char *s3;
 
-  if (
+  if (s1 == NULL)
+    {
+      s1 = "";
+    }
+  else if (s2 == NULL)
+    {
+      s2 = "";
+    }
+  else
+    {
+  for (i = 0; i != '\0'; i++)
+    {
+      s1[i] += s2[i];
+    }
+    } 
+  
+  s3 = malloc(i * sizeof(char));
+  if (s3 == NULL)
+    {
+      return (NULL);
+    }
+  for (k = 0; k != '\0'; k++)
+    {
+      s3[i] += s1[k];
+    }
+  return(s3);
+  free(s3);
+}
