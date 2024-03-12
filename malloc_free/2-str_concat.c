d75e0d51a0f6550d79ced2ba2f343413de547fa7
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-12 13:13:30 +0000

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,37 +8,41 @@ char *str_concat(char *s1, char *s2)
 {
   int i;
   int j;
-  char *s3;
-  for (i = 0; s1[i] != '\0'; i++)
+  int k;
+  int l;
+  char *s3 = NULL;
+ 
+  for (k = 0; s1[k] != '\0'; k++)
     {}
-  for (j = 0; s2[j] != '\0'; i++)
+  for (l = 0; s2[l] != '\0'; l++)
     {}
 
 
-
-  s3 = malloc(sizeof(char) * (i + j));
+  s3 = malloc((l + k + 1) * sizeof(char));
   if (s3 == NULL)
     {
       return (NULL);
     }
-
-  if (s1 == NULL)
-    {
-      s1 = '\0';
-    }
+  i = 0;
+  j = 0;
+ 
   while (s1[i] != '\0')
     {
+      if (s1 == NULL)
+	{
+	  s1 = "";
+	}
       s3[j] = s1[i];
       i++;
       j++;
     }
   i = 0;
-  if (s2 == NULL)
-    {
-      s2 = '\0';
-	}
   while (s2[i] != '\0')
     {
+      if (s2 == NULL)
+	{
+	  s2 = "";
+	}
       s3[j] = s2[i];
       i++;
       j++;
