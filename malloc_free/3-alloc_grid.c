d965762c71cf1724a6520b2801acee98707e0529
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-13 15:02:58 +0000

    save

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,47 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * alloc_grid - returns a pointer to a 2d dimensional array of integers
+ * @width: largeur
+ * @height: hauteur
+ * Return: pointer
+ */
 
 
 
 int **alloc_grid(int width, int height)
 {
-  int i,k,l;
-  int **ptp;
-  if (width <= 0 || height <= 0)
-    {
-      return (NULL);
-    }
-  
-  ptp = malloc(height * sizeof(int*));
-  if (ptp == NULL)
-    {
-      return (NULL);
-    }
-  for (i = 0; i < height; i++)
-    {
-      
-      ptp[i] = malloc(width * sizeof(int));
-      if (ptp[i] == NULL)
-	{
-	  free(ptp[i]);
-	  return (NULL);
-	}
-    }
- 
-
-   
-    
-  for (k = 0; k < height; k++)
-    {
-      for (l = 0; k < l; l++)
-	{
-      ptp[k][l] = 0;
-	}
-    }
-
-	    return (ptp);
+int i, k, l;
+int **ptp = NULL;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
 }
-
-        
-  
+ptp = malloc(height * sizeof(int *));
+if (ptp == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+ptp[i] = malloc(width * sizeof(int));
+if (ptp[i] == NULL)
+{
+free(ptp[i]);
+return (NULL);
+}
+}
+for (k = 0; k < height; k++)
+{
+for (l = 0; k < l; l++)
+{
+ptp[k][l] = 0;
+}
+}
+return (ptp);
+}
