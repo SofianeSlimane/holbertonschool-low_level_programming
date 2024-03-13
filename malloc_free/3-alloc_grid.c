a060a7a55a1444f64236aea2e9b302377700ef99
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-13 14:43:41 +0000

    save

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -6,28 +6,40 @@
 
 int **alloc_grid(int width, int height)
 {
-  int i;
+  int i,k,l;
   int **ptp;
   if (width <= 0 || height <= 0)
     {
       return (NULL);
     }
   
-  ptp = malloc(width * sizeof(int*));
- 
-  for (i = 0; i < width; i++)
-    {
-      ptp[i] = malloc(height * sizeof(int));
-    }	      
-
+  ptp = malloc(height * sizeof(int*));
   if (ptp == NULL)
     {
       return (NULL);
     }
-  
-  
-	    
-    	    
+  for (i = 0; i < height; i++)
+    {
+      
+      ptp[i] = malloc(width * sizeof(int));
+      if (ptp[i] == NULL)
+	{
+	  free(ptp[i]);
+	  return (NULL);
+	}
+    }
+ 
+
+   
+    
+  for (k = 0; k < height; k++)
+    {
+      for (l = 0; k < l; l++)
+	{
+      ptp[k][l] = 0;
+	}
+    }
+
 	    return (ptp);
 }
 
