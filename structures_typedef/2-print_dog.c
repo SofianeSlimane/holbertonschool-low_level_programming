e16bb513ed2db402ddfdcca99f949ef145515ee8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-15 22:19:23 +0000

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,15 +12,28 @@ void print_dog(struct dog *d)
 if (d == NULL)
 {
 }
-if (d->name == NULL)
-{
-printf("Name: (nil)");
-}
-if (d->age == 0 || d->owner == NULL)
-{
-printf("(nil)");
-}
+ if (d->name == NULL)
+   {
+     printf("Name: (nill)");
+   }
+ else
+   {
 printf("Name: %s\n", d->name);
+   }
+ if (d->age == 0)
+   {
+     printf("Age: (nill)");
+   }
+ else
+   {
 printf("Age: %f\n", d->age);
+   }
+ if (d->owner == NULL)
+   {
+     printf("Owner: (nill)");
+   }
+ else
+   {
 printf("Owner: %s\n", d->owner);
+   }
 }
