74fc3beb0b2d5d4baeb78c599134dc85f3d36e33
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-16 18:20:13 +0000

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,15 +9,20 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  char **p1 = &name;
-  char **p2 = &owner;
+
+  
+  dog_t *new_dog = NULL;
+  new_dog->name = name;
+  new_dog->age = age;
+  new_dog->owner = owner;
+
+  new_dog = malloc(sizeof(dog_t));
   if (new_dog == NULL)
     {
       return (NULL);
     }
-  
 
-  new_dog->p2 = owner;
-  new_dog->p1 = name;
-  new_dog.age = age;
+  return (new_dog); 
+
+
 }
