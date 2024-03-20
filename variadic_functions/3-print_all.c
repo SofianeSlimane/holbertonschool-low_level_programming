6757b2bcc0f128888ff66e2dbafbbbaddaa0134a
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-20 21:52:52 +0000

    save

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+
+
+
+void print_all(const char * const format, ...)
+{
+  int i;
+  gcc -Wall
+  printf("%s", format[i]);
+    
+}
+
+	
