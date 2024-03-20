8c0302f8cca09d3cef73e53bac26d5f84469f581
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-20 13:51:52 +0000

    save

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+  unsigned int i;
+  int somme;
+va_list args;
+  va_start(args, n);
+
+  if (n == 0)
+    {
+      return (0);
+    }
+
+  for (i = 0; i < n; i++)
+    {
+      somme  += va_arg(args, unsigned int);
+      
+    }
+  va_end(args);
+  return (somme);
+}
