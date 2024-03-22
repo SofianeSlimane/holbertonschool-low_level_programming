7f7daae426bcc896a6e62b004a929a07007f3a9f
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-22 22:06:44 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,63 +1,88 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ * print_c - prints character
+ * @args: argument
+ *
+ * Return: Nothing
+ */
 
 void print_c(va_list args)
 {
-  
-  printf("%c", va_arg(args, int));
-  
+printf("%c", va_arg(args, int));
 }
+/**
+ * print_i - print integer
+ * @args: argument
+ *
+ * Return: Nothing
+ */
 void print_i(va_list args)
 {
-  printf("%d", va_arg(args, int));
+printf("%d", va_arg(args, int));
 }
+/**
+ * print_s - print string
+ * @args: argument
+ *
+ * Return: Nothing
+ */
 void print_s(va_list args)
 {
-  if (args == NULL)
-    {
-      printf("(nil)");
-    }
-  printf("%s", va_arg(args, char*));
- 
+char *string = va_arg(args, char*);
+if (string == NULL)
+{
+printf("(nil)");
 }
+printf("%s", string);
+}
+/**
+ * print_f - print float
+ * @args: argument
+ *
+ * Return: Nothing
+ */
 void print_f(va_list args)
 {
-  printf("%f", va_arg(args, double));
-  
+printf("%f", va_arg(args, double));
 }
-	 
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments
+ *
+ * Return: Nothing
+ */
 void print_all(const char * const format, ...)
 {
-  char *separator = ", ";
-  int i, j;
-  va_list args;
-  sp_t tab[] = {
+int i, j;
+char *separator = "";
+va_list args;
+sp_t tab[] = {
 {"c", print_c},
 {"i", print_i},
 {"f", print_f},
 {"s", print_s},
 {NULL, NULL}
 };
-  i = 0;
-  j = 0;
-  va_start(args, format);
-  while (format != NULL && tab[i].sp != NULL)
-    {
-      while (format[i] != '\0' && tab[j].sp != NULL)
-	{
-	  if (*tab[i].sp == format[j])
-	    {
-	      tab[i].f(args);
-	      printf("%s", separator);
-	      
-	    }
-  
-  
-    
+i = 0;
+j = 0;
+va_start(args, format);
+while (format[i] != '\0')
+{
+j = 0;
+while (tab[j].sp != NULL)
+{
+if (*tab[j].sp == format[i])
+{
+printf("%s", separator);
+tab[j].f(args);
+separator = ", ";
+break;
 }
-    }
-      printf("\n");
+j++;
 }
-	
+i++;
+}
+printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,11 +7,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct op - Struct op
+ *
+ * @sp: specifier
+ * @f: function associated
+ */
 typedef struct sp
 {
-  char *sp;
-  void (*f)(va_list args);
-}sp_t;
+char *sp;
+void (*f)(va_list args);
+} sp_t;
 
 void print_c(va_list args);
 void print_i(va_list args);
