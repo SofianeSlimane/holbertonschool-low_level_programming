163ba02c1b91c500415569b71941a875a50412e8
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-30 23:41:58 +0000

    Got correct output and also added betty style and doc

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,29 +1,29 @@
 #include "lists.h"
-
+/**
+ * print_list - prints elements of a singly linked list
+ * @h: First node which points to another node
+ * Return: number of nodes
+ */
 
 
 
 size_t print_list(const list_t *h)
-{       
-	int i;
+{
 	size_t numberOfNodes;
 	const list_t *pointer;
+
 	pointer = malloc(sizeof(const list_t));
 	pointer = h;
-	if (pointer->str == NULL)
+	for (numberOfNodes = 0; pointer != NULL; numberOfNodes++)
 	{
-		printf("[0] (nil)");
-		
-	}
-        for (i = 0; pointer != NULL; i++)
-	{
+		if (pointer->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			pointer = pointer->next;
+			numberOfNodes++;
+		}
 		printf("[%u] %s\n", pointer->len, pointer->str);
-		
 		pointer = pointer->next;
-		numberOfNodes++;
-		
-		
-	}
-	
+		}
 	return (numberOfNodes);
 }
