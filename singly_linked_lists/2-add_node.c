98a7ae09a927954c3117e7c73225e6b1c8045708
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-31 16:18:26 +0000

    Got length of string with for loop instead of strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,14 +6,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	 char *stringCopy; /*Declares a variable that will holds the value of str*/
+	unsigned int i; 
+	char *stringCopy; /*Declares a variable that will holds the value of str*/
 	 list_t *newNode; /*Declares a pointer to a new node*/
          
 
 	 stringCopy = strdup(str); /*Assigns a copy of str to stringCopy*/
+	 for (i = 0; stringCopy[i] != '\0'; i++)
+	 {
+	 }
 	 newNode = malloc(sizeof(list_t)); /*µAllocates memory for our new nod*/
 	 newNode->str = stringCopy;/*Assigns a string value to our new node*/
-	 newNode->len = strlen(stringCopy);
+	 newNode->len = i;
 	 newNode->next = *head; /*Our new node will now points to the node head points to */
 	 *head = newNode; /*Changes the node head points to, to our new node*/
 	 return (newNode);
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -23,7 +23,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 
 
-int _strlen(char *s);
+
 
 
 
