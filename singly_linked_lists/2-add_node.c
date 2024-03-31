8bbd7ef1b524b6b4c2242860e165d3dfd7098cdb
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-31 16:03:48 +0000

    Wrote a fuction that changes the first node of a singly linked list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,7 +6,17 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	const char *copyString;
-	copyString = strdup(str);
-	list_t *pointer =
+	 char *stringCopy; /*Declares a variable that will holds the value of str*/
+	 list_t *newNode; /*Declares a pointer to a new node*/
+         
+
+	 stringCopy = strdup(str); /*Assigns a copy of str to stringCopy*/
+	 newNode = malloc(sizeof(list_t)); /*µAllocates memory for our new nod*/
+	 newNode->str = stringCopy;/*Assigns a string value to our new node*/
+	 newNode->len = strlen(stringCopy);
+	 newNode->next = *head; /*Our new node will now points to the node head points to */
+	 *head = newNode; /*Changes the node head points to, to our new node*/
+	 return (newNode);
+}
+
 
