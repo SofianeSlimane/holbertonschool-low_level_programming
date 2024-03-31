52a18a4a71244cfdcb044ded9fdfabe2da51d195
SofianeSlimane <sofianeslimane24@gmail.com>
2024-03-31 16:44:49 +0000

    Added betty style to c file

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,30 +1,34 @@
 #include "lists.h"
-
+/**
+ * add_node - adds a new node at the beginning of a singly linked list
+ * @head: a pointer to a pointer head that points to the first node of the list
+ * @str: string to be copied in new node
+ * Return: the adress of the new node
+ */
 
 
 
 
 list_t *add_node(list_t **head, const char *str)
 {
-	unsigned int i; 
-	char *stringCopy; /*Declares a variable that will holds the value of str*/
-	 list_t *newNode; /*Declares a pointer to a new node*/
-         
+	unsigned int i;
+	char *stringCopy;/*Declares a variable that will holds the value of str*/
+	list_t *newNode;/*Declares a pointer to a new node*/
 
-	 stringCopy = strdup(str); /*Assigns a copy of str to stringCopy*/
-	 for (i = 0; stringCopy[i] != '\0'; i++)
-	 {
-	 }
-	 newNode = malloc(sizeof(list_t)); /* Allocates memory for our new node*/	
-	 newNode->str = stringCopy;
-	 newNode->len = i;
-	 newNode->next = *head; /*Our new node will now points to the node head points to*/
-	 *head = newNode; /*Changes the node head points to, to our new node*/
-	 if (*head == NULL)
-	 {
-		 return (NULL);
+	stringCopy = strdup(str);/*Assigns a copy of str to stringCopy*/
+	for (i = 0; stringCopy[i] != '\0'; i++)
+	{
 	}
-	 return (newNode);
+	newNode = malloc(sizeof(list_t));/* Allocates memory for our new node*/
+	newNode->str = stringCopy;
+	newNode->len = i;
+	newNode->next = *head;/*node will now points to the node head points to*/
+	*head = newNode;/*Changes the node head points to, to our new node*/
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	return (newNode);
 }
 
 
