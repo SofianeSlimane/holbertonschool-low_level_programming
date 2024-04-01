b02eac96b969fafd93567105b985786cb17a10dd
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-01 14:49:57 +0000

    Still 1 alloc to be freed

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,15 +5,15 @@
 void free_list(list_t *head)
 {
 list_t *pointer;
-pointer = head;
-while ( pointer != NULL && head != NULL)
+
+while ( head != NULL)
 {
 	pointer = head;
 	head = head->next;
 
 	free(pointer->str);
-	
 	free(pointer);
 	
 }
+
 }
