8f1d5854a7ee4bf86c0d40ff2eee5694cca94d4e
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-01 17:41:43 +0000

    Assigned str value directly to node instead of creating a stringCopy variable

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,27 +12,25 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int i;
-	char *stringCopy;/*Declares a variable that will holds the value of str*/
 	list_t *newNode;/*Declares a pointer to a new node*/
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	stringCopy = strdup(str);
-	for (i = 0; stringCopy[i] != '\0'; i++)
-	{
-	}
 	newNode = malloc(sizeof(list_t));/* Allocates memory for our new node*/
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-	newNode->str = stringCopy;
+	newNode->str = strdup(str);
 	if (newNode->str == NULL)
 	{
 		free(newNode);
 		return (NULL);
+	}
+	for (i = 0; newNode->str[i] != '\0'; i++)
+	{
 	}
 	newNode->len = i;
 	newNode->next = *head;/*node will now points to the node head points to*/
