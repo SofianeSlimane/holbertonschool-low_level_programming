bfd7f1c5f338e22aac05a63046c1db6c351808b9
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-04 09:52:42 +0000

    Removed the second pointer that travels from the end of the list, it was unecessary

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,10 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode;
 	dlistint_t *ptr;
-	dlistint_t *ptr2;
 	unsigned int i;
-	unsigned int j;
-	unsigned int k;
 
 	if (*h == NULL)
 	{
@@ -29,32 +26,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->prev = NULL;
 	newNode->next = NULL;
-	
+
 	ptr = *h;
-	ptr2 = *h;
 	i = 0;
-	while (*h != NULL)
+	while (ptr != NULL)
 	{
-		ptr2 = ptr2->next;
+		if (i == (idx - 1))
+		{
+			break;
+		}
+		ptr = ptr->next;
 		i++;
 	}
-	j = 0;
-	while (j != (idx - 1))
-	{
-		ptr = ptr->next;
-		j++;
-	}
-	k = i;
-	while (k != idx)
-	{
-		ptr2 = ptr2->prev;
-		k--;
-	}
-	newNode->next = ptr2;
+
+	newNode->next = ptr->next;
 	newNode->prev = ptr;
-
-	ptr2->prev = newNode;
 	ptr->next = newNode;
-	return (newNode);
 
+	return (newNode);
 }
