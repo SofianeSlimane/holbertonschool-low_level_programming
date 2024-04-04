1c71e99efdf801969b6f9150c23e175ac01801f3
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-04 12:07:23 +0000

    Added if head is != null condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	newNode->n = n;
+	newNode->next = NULL;
+	newNode->prev = NULL;
 	if (*h == NULL)
 	{
 		*h = newNode;
