36fcf61a86b29c534f365af00d8ed2496b5e0164
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-04 12:54:20 +0000

    Removed condition

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newNode;
 	dlistint_t *ptr;
 	unsigned int i;
-	if (*h == NULL)
+	if (h == NULL)
 	{
 		return (NULL);
 	}	
