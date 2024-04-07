76acba1965c88f09569f2c71b2ad78c50e121cd2
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-07 18:53:54 +0000

    Added condition if text_content == NULL return -1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,13 +16,18 @@ int create_file(const char *filename, char *text_content)
 	int fd, wr, length;
 
 
-	if (filename == NULL || text_content)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC,  S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		close(fd);
 		return (-1);
 	}
 	length = 0;
