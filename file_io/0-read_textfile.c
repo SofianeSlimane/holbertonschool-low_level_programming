42353f41a51d1ba964e16775a8dfbdca7a7de3bd
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-07 17:10:10 +0000

    Wrote a function that reads a text file and writes its content to the std output, work in progress

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,15 +9,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rd, wr;
 
 	char *buf;
-
+	if (filename == NULL)
+	{
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
-
+	if (fd == -1)
+	{
+		return (0);
+	}
 	buf = malloc(sizeof(char) * letters);
-
+	if (buf == NULL)
+	{
+		return (0);
+	}
 	rd = read(fd, buf, letters);
-
+	if (rd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	wr = write(STDOUT_FILENO, buf, rd);
-
+	if (wr == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	close(fd);
 
 	return (rd);
