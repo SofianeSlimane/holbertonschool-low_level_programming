350b160f04a4eb81b893d3ffadb1e275e0a93a71
SofianeSlimane <sofianeslimane24@gmail.com>
2024-04-07 17:35:11 +0000

    Added Betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * read_textfile - reads the content of a file and prints to stdout
+ * @filename: string that holds the name of the file we want to read from
+ * @letters: number of characters to read in the file
+ * Return: the numbers of bytes (characters) read
+ */
 
 
 
@@ -7,22 +12,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t rd, wr;
-
 	char *buf;
+
 	if (filename == NULL)
-	{
 		return (0);
-	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
-	{
 		return (0);
-	}
 	rd = read(fd, buf, letters);
 	if (rd == -1)
 	{
@@ -38,7 +37,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	close(fd);
-
 	return (rd);
 }
-
