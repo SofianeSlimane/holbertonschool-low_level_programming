c261329a0bcb45fb21ef085c48224d1485686392
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-11 19:40:19 +0200

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,39 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * times_table - print times table up to 9
  *
  * Return: Always 0.
  */
 
-
 void  times_table(void)
 {
-  int i, j, first_num, second_num, result;
-  for (i = 0; i < 10; i++) {
+int i, j, first_num, second_num, result;
+
+for (i = 0; i < 10; i++)
+{
 	_putchar('0');
 	_putchar(',');
-  	for (j = 1; j < 10; j++) {
+		for (j = 1; j < 10; j++)
+		{
 		result = i * j;
-	
-		if (result < 10)  {
+
+		if (result < 10)
+		{
 			_putchar(' ');
 			_putchar(' ');
 			_putchar('0' + result);
 		}
-		else if (result > 9) {
+		else if (result > 9)
+		{
 			_putchar(' ');
 			first_num = result / 10;
 			second_num = result % 10;
 			_putchar('0' + first_num);
 			_putchar('0' + second_num);
 		}
-		if (j < 9) {
+		if (j < 9)
+		{
 			_putchar(',');
 		}
 	}
 	_putchar('\n');
-  }
-
 }
-
+}
