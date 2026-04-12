6984c84a9bc10dd768c01c926501bf04154194a5
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-12 15:40:12 +0200

    Task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * _strlen - get string's length
+ * @s: string
+ * Return: len of string as integer
+ */
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'  ; i++)
+{
+}
+return (i);
+}
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ */
 void rev_string(char *s)
 {
-  int taille;
-  int i;
-  for (taille = 0; s[taille] != '\0'; taille++)
-    {
-    }
-
-  if (taille > 0)
-    {
-  for (i = taille -1; s[i] != '\0'; i--)
-    {
-      
-    }
-    }
+int  i, j;
+char tmp;
+j = _strlen(s) - 1;
+for (i = 0; i != j; i++)
+{
+tmp = s[i];
+s[i] = s[j];
+s[j] = tmp;
+j--;
+}
 }
