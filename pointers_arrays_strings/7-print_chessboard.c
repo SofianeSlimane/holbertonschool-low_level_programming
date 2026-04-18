7442b48a13bd5840eb2205bf5fb1cb416846505c
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-18 20:22:38 +0200

    Task 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,21 +8,21 @@
 
 void print_chessboard(char (*a)[8])
 {
-int i;
-int j;
-for (i = 0; i < 8 ; i++)
-{
-for (j = 0; j < 8; j++)
-{
-if (j == 7)
-{
-_putchar('\n');
-}
-else
-{
-_putchar(a[i][j]);
-}
-}
-}
-_putchar('\n');
+	int i;
+	int j;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			if (j == 8)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(a[i][j]);
+			}
+		}
+	}
 }
