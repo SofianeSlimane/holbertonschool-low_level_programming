e5d34a4961cd16a93b7c29e8fad5e2f72251e50d
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-18 19:31:44 +0200

    Task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,52 @@
 #include "main.h"
 /**
+ * _strlen - check the code
+ *@s: chaine de caracteres
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'  ; i++)
+{
+}
+
+return (i);
+}
+/**
  * _strstr - locates substring
  * @haystack: string
  * @needle: characters to be matched
  * Return: pointer to beginning of located substring
  */
-
-
-
-
 char *_strstr(char *haystack, char *needle)
 {
-  int i;
-  int j;
+	int i, j, count, len_needle;
+	char *first_occur;
 
-  for (i = 0; haystack[i] != '\0'; i++)
-    {
-      for (j = 0; i >= j; j++)
+	count = 0;
+	len_needle = _strlen(needle);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-	  if (needle[i] == *haystack)
-	    {
-	      haystack += i;
-	      return (haystack);
-	    }
-	  
+		first_occur = haystack + i;
+		for (j = 0; len_needle != count; j++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				count++;
+				if (count == len_needle)
+					return (first_occur);
+				i++;
+			}
+			else
+			{
+				count = 0;
+				break;
+			}
+
+		}
+
 	}
-    }
-  return ('\0');
+	return ('\0');
 }
