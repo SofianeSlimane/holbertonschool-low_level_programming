8e791b2606995504437870bd9311c2805e9887e5
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-23 20:28:41 +0200

    Task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,18 +1,39 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * divide - divide
+ * @n: integer to divide
+ * @d: dividor
+ * Return: divided number
+ */
+
+int divide(int n, int d)
+{
+	if (d == 1)
+		return (1);
+	if (n % d == 0)
+		return (0);
+
+	return (divide(n, d - 1));
+}
+
+
 /**
  * is_prime_number - look for prime numbers
  * @n: number
  * Return: 1 if number is primer or otherwise 0
  */
-
-
-
 int is_prime_number(int n)
 {
-if (n > 1 && n % n == 0 && n % 1 == 0 && n % 2 != 0 && n % 5 != 0)
-{
-return (1);
-is_prime_number(n);
-}
-return (0);
+	int indicator;
+
+	if (n <= 1)
+		return (0);
+
+	indicator = divide(n, n - 1);
+
+	if (indicator == 0)
+		return (0);
+
+	return (1);
 }
