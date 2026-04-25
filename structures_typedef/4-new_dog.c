fe52d6517a1b82e81ed31c137eb815d930809a54
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-25 22:09:56 +0200

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,6 +8,37 @@
  * @owner: owner
  * Return: pointer to new dog
  */
+/**
+ * _strdup - copy a string taken as a parameter and return it
+ * @str: pointer to string to be copied
+ * Return: pointer to string containing copy of given string
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *str2;
+	int j;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0'; j++)
+	{}
+	str2 = malloc(sizeof(char) * j + 1);
+	
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= j; i++)
+	{
+		str2[i] = str[i];
+	}
+
+	return (str2);
+	free(str2);
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -17,11 +48,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = _strdup(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = _strdup(owner);
 
 	return (new_dog);
 }
