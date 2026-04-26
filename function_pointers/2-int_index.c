c58f143260e9a1d740513fbda8b6675f536ad2d8
SofianeSlimane <sofianeslimane24@gmail.com>
2026-04-26 21:03:24 +0200

    Task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,19 +11,18 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
-if (size <= 0)
-{
-return (-1);
-}
-i = 0;
-while (i <= size && cmp(array[i]) == 0)
-{
-i++;
-}
-if (cmp == NULL)
-{
-return (0);
-}
-return (i);
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
 }
